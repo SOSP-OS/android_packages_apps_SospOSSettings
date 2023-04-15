bananadroid/android_vendor_support

support: Introduce AppListPreference for a much cleaner look
Something like HeadsUp Blacklist or Gaming Mode show
an app list to the user of those which have been
selected in the preference. This is where there is no
control over the sizing or layout of things. See the
following image:

BEFORE: imgur.com/7muwWZ7.jpg

To address this, we will simply extend the Preference
class and add a layout for it to show things properly.
See the following image:

AFTER:  imgur.com/DzgVOjX.jpg

To utilize this, refer to the following patch:
https://review.aosip.dev/c/AOSIP/platform_packages_apps_OwlsNest/+/17438

Signed-off-by: calebcabob <calphonic@gmail.com>
Change-Id: I4563743b75a6fc305bc77f903f0c3d1391d77973

src/com/banana/support/preferences/AppListPreference.java
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2021 AOSiP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.banana.support.preferences;
+
+import android.content.Context;
+import androidx.preference.Preference;
+import android.util.AttributeSet;
+
+import com.banana.support.R;
+
+public class AppListPreference extends Preference {
+
+    public AppListPreference(Context context, AttributeSet attrs,
+                               int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    public AppListPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public AppListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public AppListPreference(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        setLayoutResource(R.layout.app_list_preference_view);
+    }
+}

SOSP-OS/android_packages_apps_SospOSSettings

src/com/sospos/support/preferences/AppListPreference.java
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2021 AOSiP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sospos.support.preferences;
+
+import android.content.Context;
+import androidx.preference.Preference;
+import android.util.AttributeSet;
+
+import com.sospos.support.R;
+
+public class AppListPreference extends Preference {
+
+    public AppListPreference(Context context, AttributeSet attrs,
+                               int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    public AppListPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public AppListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public AppListPreference(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        setLayoutResource(R.layout.app_list_preference_view);
+    }
+}